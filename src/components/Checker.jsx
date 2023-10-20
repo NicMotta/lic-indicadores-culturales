9cb3122d2cdc23f09d811af4d06cb1f8f720a715
Nic Motta <nicolas.motta.contractor@gdevtools.com>
2023-10-19 21:59:39 -0300

    removed return

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -2,10 +2,10 @@ export default function Checker ({code, storage, redirect, login}) {
   const storageCode = localStorage.getItem(storage);
 
   if (!storageCode) {
-    return window.location.href = login;
+    window.location.href = login;
   }
 
   if (storageCode !== code) {
-    return window.location.href = redirect;
+    window.location.href = redirect;
   }
 };
