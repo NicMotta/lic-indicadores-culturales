0d7b0e197dd818effee5f3102b0a54db929f90b5
Nic Motta <nicolas.motta.contractor@gdevtools.com>
2023-12-16 09:17:07 -0300

    se ocultan los resultados

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,27 +1,27 @@
 export const URL_LINKS = {
-  home: '/',
+  home: "/",
   results: {
-    residencias: '/2023/resultados-residencias',
-    residentes: '/2023/resultados-residentes',
+    residencias: "/2023/resultados-residencias",
+    residentes: "/2023/resultados-residentes",
   },
   encuestas: {
-    residencias: '/formulario-residencias',
-    residentes: '/formulario-residentes',
+    residencias: "/formulario-residencias",
+    residentes: "/formulario-residentes",
   },
-  quienesSomos: '/quienes-somos',
-  categorias: '/instrucciones-de-uso/#categorias',
-  manifiesto: 'quienes-somos/#manifiesto',
-  buenasPracticas: '/quienes-somos/#buenas-practicas',
-  alianzas: '/quienes-somos/#alianzas',
-  instruccionesDeUso: '/instrucciones-de-uso',
-  redQuincho: 'https://redquincho.ar/',
-  loginResidencias: '/login-residencias',
-  loginResidentes: '/login-residentes',
+  quienesSomos: "/quienes-somos",
+  categorias: "/instrucciones-de-uso/#categorias",
+  manifiesto: "quienes-somos/#manifiesto",
+  buenasPracticas: "/quienes-somos/#buenas-practicas",
+  alianzas: "/quienes-somos/#alianzas",
+  instruccionesDeUso: "/instrucciones-de-uso",
+  redQuincho: "https://redquincho.ar/",
+  loginResidencias: "/login-residencias",
+  loginResidentes: "/login-residentes",
 };
 
-export const SHOW_RESULTS = true;
+export const SHOW_RESULTS = false;
 
-export const CODIGO_RESIDENCIAS = 'LIC2023'
-export const CODIGO_RESIDENTES = 'RESIDENTES2023'
-export const STORAGE_RESIDENCIA = 'loginCodeResidencia'
-export const STORAGE_RESIDENTE = 'loginCodeResidente'
+export const CODIGO_RESIDENCIAS = "LIC2023";
+export const CODIGO_RESIDENTES = "RESIDENTES2023";
+export const STORAGE_RESIDENCIA = "loginCodeResidencia";
+export const STORAGE_RESIDENTE = "loginCodeResidente";
